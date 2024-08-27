051b71582a0061911228e4a74069931b5de16650
RoD <rodcejas@gmail.com>
2024-08-27 03:14:22 -0300

    Add Swagger

--- Atm-Rod-Api/Program.cs
+++ Atm-Rod-Api/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ATM TEST", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,5 +19,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("v1/swagger.json", "ATM TEST");
+});
 
 app.Run();
